b261fa0ce15d37ddbb3ce966dfc811214f3f332d
keffalyssa <keffalyssak@gmail.com>
2026-07-14 07:03:20 -0700

    Fix pycodestyle E501 error

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,15 +2,14 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    rom_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                'C': 100, 'D': 500, 'M': 1000}
+    d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
-    prev_val = 0
+    prev = 0
     for char in reversed(roman_string):
-        val = rom_dict.get(char, 0)
-        if val < prev_val:
+        val = d.get(char, 0)
+        if val < prev:
             num -= val
         else:
             num += val
-        prev_val = val
+        prev = val
     return num
