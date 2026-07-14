e393a7c93214746bbd7efc3c06acfd46d8076bb2
keffalyssa <keffalyssak@gmail.com>
2026-07-14 06:58:38 -0700

    Add roman_to_int conversion function

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    rom_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num = 0
+    prev_val = 0
+    for char in reversed(roman_string):
+        val = rom_dict.get(char, 0)
+        if val < prev_val:
+            num -= val
+        else:
+            num += val
+        prev_val = val
+    return num
