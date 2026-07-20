ec00e35f305dd3de8608c418da614bdb34c856fc
keffalyssa <keffalyssak@gmail.com>
2026-07-20 02:28:42 -0700

    Fix exact with slice for 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = ("Python is an interpreted, interactive, "
        "object-oriented programming language")
-str = str[39:66] + str[54:55] + str[102:106] + str[54:55] + str[0:6]
+str = str[39:66] + str[31:33] + str[10:11] + str[51:53] + str[0:6]
 print(str)
