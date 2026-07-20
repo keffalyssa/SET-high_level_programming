d0b629a3043162424d948380761c6c6f02d64f78
keffalyssa <keffalyssak@gmail.com>
2026-07-20 02:11:05 -0700

    Fix line length and add with in 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language"
-str = str[39:66] + str[54:55] + str[0:6]
+str = ("Python is an interpreted, interactive, "
+      "object-oriented programming language")
+str = str[39:66] + " with " + str[0:6]
 print(str)
