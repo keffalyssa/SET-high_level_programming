6884d7c7490d5fed937cb392b4284a0d97f9d520
keffalyssa <keffalyssak@gmail.com>
2026-07-20 02:08:33 -0700

    Fix 8-concat_edges.py script constraints

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language"
-print(f"{str[39:66]} {str[30:38]}{str[6:10]}{str[0:6]}")
+str = str[39:66] + str[54:55] + str[0:6]
+print(str)
