81c26b8b8fcbb3414200cd722959eef6a6f19823
keffalyssa <keffalyssak@gmail.com>
2026-07-20 01:53:06 -0700

    Update 8-concat_edges.py script

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language"
-str = str[39:66] + str[30:38] + str[0:6]
-print(str)
+print(f"{str[39:66]} {str[30:38]}{str[6:10]}{str[0:6]}")
