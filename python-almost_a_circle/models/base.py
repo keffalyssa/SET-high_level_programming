4316c27fc258673d7b76a9d0718ab48a3fde312b
keffalyssa <keffalyssak@gmail.com>
2026-08-06 04:12:30 -0700

    Fix Base class automatic ID and unit tests

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -3,11 +3,22 @@
 
 
 class Base:
-    """Represent the base model."""
+    """Represent the base model.
+
+    Represents the base for all other classes in the project.
+
+    Attributes:
+        __nb_objects (int): The number of instantiated Bases.
+    """
+
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Initialize a new Base."""
+        """Initialize a new Base.
+
+        Args:
+            id (int): The identity of the new Base.
+        """
         if id is not None:
             self.id = id
         else:
@@ -16,8 +27,16 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Returns JSON string representation."""
-        import json
+        """Returns the JSON string representation of list_dictionaries."""
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
+        import json
         return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns the list of the JSON string representation json_string."""
+        if json_string is None or json_string == "":
+            return []
+        import json
+        return json.loads(json_string)
