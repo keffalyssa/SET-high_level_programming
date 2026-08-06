9364fdf6d998dfcb93c5d8011834776b5b25b125
keffalyssa <keffalyssak@gmail.com>
2026-08-06 03:33:47 -0700

    Update test_base.py for automatic ID test

--- python-almost_a_circle/tests/test_base.py
+++ python-almost_a_circle/tests/test_base.py
@@ -7,20 +7,20 @@ from models.base import Base
 class TestBase(unittest.TestCase):
     """Unittests for testing the Base class."""
 
-    def test_Base_id(self):
+    def test_nb_instance(self):
         """Test Base() for assigning automatically an ID."""
         b1 = Base()
         b2 = Base()
         self.assertEqual(b1.id, 1)
         self.assertEqual(b2.id, 2)
 
-    def test_id_custom(self):
-        """Test Base with given ID."""
-        b = Base(89)
-        self.assertEqual(b.id, 89)
+    def test_id(self):
+        """Test custom ID assignment."""
+        b = Base(98)
+        self.assertEqual(b.id, 98)
 
-    def test_to_json_string_none(self):
-        """Test Base.to_json_string(None)."""
+    def test_to_json_string(self):
+        """Test to_json_string static method."""
         self.assertEqual(Base.to_json_string(None), "[]")
 
 
