5313a98d633264f96ddede33e6f71e36724432f6
keza teta keffa lyssa <keffalyssak@gmail.com>
2026-08-07 11:02:17 +0200

    Update base.py

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,37 +1,42 @@
 #!/usr/bin/python3
-"""Defines the Base model class."""
+"""Defines a Base model class."""
+import json
 
 
 class Base:
-    """Represent the base model.
+    """Represent the base model for all other classes in this project."""
 
-    Attributes:
-        __nb_instances (int): The number of instantiated Bases.
-        id (int): A unique identifier.
-    """
-
-    __nb_instances = 0
+    __nb_objects = 0
 
     def __init__(self, id=None):
         """Initialize a new Base."""
         if id is not None:
             self.id = id
         else:
-            Base.__nb_instances += 1
-            self.id = Base.__nb_instances
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
     @staticmethod
-    def to_json_string(list_dictionaries):
+    to_json_string(list_dictionaries):
         """Return the JSON string representation of list_dictionaries."""
-        import json
-        if list_dictionaries is None or list_dictionaries == []:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON string representation of list_objs to a file."""
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                jsonfile.write(Base.to_json_string(list_dicts))
 
     @staticmethod
     def from_json_string(json_string):
         """Return the list of the JSON string representation json_string."""
-        import json
         if json_string is None or json_string == "":
             return []
         return json.loads(json_string)
