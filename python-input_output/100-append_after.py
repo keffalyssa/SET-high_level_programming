39278ded97ae312fb24b80cbf02f3454829b49cc
keffalyssa <keffalyssak@gmail.com>
2026-08-10 05:02:00 -0700

    Add 100-append_after.py

---
+++ python-input_output/100-append_after.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines a text file insertion function."""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Inserts a line of text after each line containing a specific string."""
+    text = ""
+    with open(filename, "r", encoding="utf-8") as f:
+        for line in f:
+            text += line
+            if search_string in line:
+                text += new_string
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
