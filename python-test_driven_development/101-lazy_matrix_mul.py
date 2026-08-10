ba5373592b74af3dda4d6e1e841a88b465d16b40
keffalyssa <keffalyssak@gmail.com>
2026-08-10 07:07:47 -0700

    Add lazy_matrix_mul using numpy and its tests

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,55 +1,8 @@
 #!/usr/bin/python3
-"""Defines a matrix multiplication function using NumPy with validation."""
+"""Defines a matrix multiplication function using NumPy."""
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """Multiplies two matrices by using the NumPy module.
-
-    Args:
-        m_a (list of lists of ints/floats): The first matrix.
-        m_b (list of lists of ints/floats): The second matrix.
-
-    Returns:
-        numpy.ndarray: The resulting matrix after multiplication.
-    """
-    if not isinstance(m_a, list):
-        raise TypeError("m_a must be a list")
-    if not isinstance(m_b, list):
-        raise TypeError("m_b must be a list")
-
-    for row in m_a:
-        if not isinstance(row, list):
-            raise TypeError("m_a must be a list of lists")
-    for row in m_b:
-        if not isinstance(row, list):
-            raise TypeError("m_b must be a list of lists")
-
-    if m_a == [] or m_a == [[]]:
-        raise ValueError("m_a can't be empty")
-    if m_b == [] or m_b == [[]]:
-        raise ValueError("m_b can't be empty")
-
-    for row in m_a:
-        for x in row:
-            if not isinstance(x, (int, float)):
-                raise TypeError("m_a should contain only integers or floats")
-    for row in m_b:
-        for x in row:
-            if not isinstance(x, (int, float)):
-                raise TypeError("m_b should contain only integers or floats")
-
-    first_len = len(m_a[0])
-    for row in m_a:
-        if len(row) != first_len:
-            raise TypeError("each row of m_a must should be of the same size")
-
-    second_len = len(m_b[0])
-    for row in m_b:
-        if len(row) != second_len:
-            raise TypeError("each row of m_b must should be of the same size")
-
-    if len(m_a[0]) != len(m_b):
-        raise ValueError("m_a and m_b can't be multiplied")
-
+    """Multiplies two matrices using the NumPy module."""
     return np.matmul(m_a, m_b)
