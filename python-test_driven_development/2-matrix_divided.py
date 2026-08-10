c7dd17710cf3a81b4d1e532ab74f936710976084
keffalyssa <keffalyssak@gmail.com>
2026-08-10 05:47:54 -0700

    Fix pycodestyle and ensure correct division handling for inf/float

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -23,14 +23,16 @@ def matrix_divided(matrix, div):
             not all(len(row) > 0 for row in matrix) or
             not all(all(isinstance(ele, (int, float)) for ele in row)
                     for row in matrix)):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+            "matrix must be a matrix (list of lists) of integers/floats"
+        )
 
     row_length = len(matrix[0])
     for row in matrix:
         if len(row) != row_length:
             raise TypeError("Each row of the matrix must have the same size")
 
-    if not isinstance(div, (int, float)):
+    if not isinstance(div, (int, float)) or isinstance(div, bool):
         raise TypeError("div must be a number")
 
     if div == 0:
