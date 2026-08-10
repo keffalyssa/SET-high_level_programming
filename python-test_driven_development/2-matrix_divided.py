905b6aa54e8eeaf3d4bfe2ff7a8112b1940574a4
keffalyssa <keffalyssak@gmail.com>
2026-08-10 06:19:56 -0700

    Fix pycodestyle line length limits and standard matrix division

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -2,7 +2,7 @@
 """Defines a matrix division function."""
 
 
-def matrix_divided(matrix=None, div=None):
+def matrix_divided(matrix, div):
     """Divides all elements of a matrix by div.
 
     Args:
@@ -18,23 +18,13 @@ def matrix_divided(matrix=None, div=None):
                    or if div is not a number.
         ZeroDivisionError: If div is equal to 0.
     """
-    if matrix is None:
-        raise TypeError(
-            "matrix_divided() missing 2 required positional arguments: 'matrix' and 'div'"
-        )
-    if div is None:
-        raise TypeError(
-            "matrix_divided() missing 1 required positional argument: 'div'"
-        )
-
+    err_matrix = "matrix must be a matrix (list of lists) of integers/floats"
     if (not isinstance(matrix, list) or matrix == [] or
             not all(isinstance(row, list) for row in matrix) or
             not all(len(row) > 0 for row in matrix) or
             not all(all(isinstance(ele, (int, float)) for ele in row)
                     for row in matrix)):
-        raise TypeError(
-            "matrix must be a matrix (list of lists) of integers/floats"
-        )
+        raise TypeError(err_matrix)
 
     row_length = len(matrix[0])
     for row in matrix:
@@ -47,4 +37,4 @@ def matrix_divided(matrix=None, div=None):
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    return [[0.0 if div == float('inf') or div == float('-inf') else round(ele / div, 2) for ele in row] for row in matrix]
+    return [[round(ele / div, 2) for ele in row] for row in matrix]
