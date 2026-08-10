b757b77130b9195bb1ff9a629da11eb76c162d4d
keffalyssa <keffalyssak@gmail.com>
2026-08-10 06:58:26 -0700

    Fix strict sequential validation order for m_a then m_b

--- python-test_driven_development/100-matrix_mul.py
+++ python-test_driven_development/100-matrix_mul.py
@@ -14,34 +14,29 @@ def matrix_mul(m_a, m_b):
     """
     if not isinstance(m_a, list):
         raise TypeError("m_a must be a list")
-    if not isinstance(m_b, list):
-        raise TypeError("m_b must be a list")
-
     if not all(isinstance(row, list) for row in m_a):
         raise TypeError("m_a must be a list of lists")
-    if not all(isinstance(row, list) for row in m_b):
-        raise TypeError("m_b must be a list of lists")
-
     if m_a == [] or m_a == [[]]:
         raise ValueError("m_a can't be empty")
-    if m_b == [] or m_b == [[]]:
-        raise ValueError("m_b can't be empty")
-
     for row in m_a:
         for ele in row:
             if not isinstance(ele, (int, float)):
                 raise TypeError("m_a should contain only integers or floats")
-
-    for row in m_b:
-        for ele in row:
-            if not isinstance(ele, (int, float)):
-                raise TypeError("m_b should contain only integers or floats")
-
     row_len_a = len(m_a[0])
     for row in m_a:
         if len(row) != row_len_a:
             raise TypeError("each row of m_a must be of the same size")
 
+    if not isinstance(m_b, list):
+        raise TypeError("m_b must be a list")
+    if not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_b must be a list of lists")
+    if m_b == [] or m_b == [[]]:
+        raise ValueError("m_b can't be empty")
+    for row in m_b:
+        for ele in row:
+            if not isinstance(ele, (int, float)):
+                raise TypeError("m_b should contain only integers or floats")
     row_len_b = len(m_b[0])
     for row in m_b:
         if len(row) != row_len_b:
