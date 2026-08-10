bce660da022414f03f62aed7356e5cd69c8a1d3d
keffalyssa <keffalyssak@gmail.com>
2026-08-10 02:24:55 -0700

    Add 1-write_file.py

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a file-writing function."""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a UTF-8 text file and returns the number of characters written."""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
