127096e57af8802146ce9057be6686d08dab2ebc
keffalyssa <keffalyssak@gmail.com>
2026-08-12 02:14:04 -0700

    Add model_city.py and 14-model_city_fetch_by_state.py

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Python file that contains the class definition of a City.
+"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """City class that links to the MySQL table cities."""
+    __tablename__ = 'cities'
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        autoincrement=True,
+        unique=True
+    )
+    name = Column(
+        String(128),
+        nullable=False
+    )
+    state_id = Column(
+        Integer,
+        ForeignKey('states.id'),
+        nullable=False
+    )
