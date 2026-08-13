d4a09f3acf8b040b8bf359344e52a56a74bca918
keffalyssa <keffalyssak@gmail.com>
2026-08-13 04:58:01 -0700

    Add task 10-my_github.py to fetch GitHub user ID using Basic Auth

---
+++ python-network_1/10-my_github.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Module 10-my_github
+Takes GitHub credentials (username and personal access token)
+and uses the GitHub API to display your id.
+"""
+import sys
+import requests
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    token = sys.argv[2]
+    
+    url = 'https://api.github.com/user'
+    response = requests.get(url, auth=(username, token))
+    
+    try:
+        json_response = response.json()
+        print(json_response.get('id'))
+    except ValueError:
+        print(None)
