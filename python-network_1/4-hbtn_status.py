a4db3fe1bc08167871aa0d4b40d558f272c0e3ba
keffalyssa <keffalyssak@gmail.com>
2026-08-13 04:40:07 -0700

    Add task 4-hbtn_status.py to fetch status using requests package

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module 4-hbtn_status
+Fetches https://alx-intranet.hbtn.io/status using the requests package
+"""
+import requests
+
+
+if __name__ == '__main__':
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    content = response.text
+    print("Body response:")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
