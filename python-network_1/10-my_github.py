82238d213701dffc5ac7ad60570017769c81724d
keffalyssa <keffalyssak@gmail.com>
2026-08-13 04:59:30 -0700

    Fix Pycodestyle trailing whitespace in blank lines for 10-my_github.py

--- python-network_1/10-my_github.py
+++ python-network_1/10-my_github.py
@@ -11,10 +11,8 @@ import requests
 if __name__ == '__main__':
     username = sys.argv[1]
     token = sys.argv[2]
-    
     url = 'https://api.github.com/user'
     response = requests.get(url, auth=(username, token))
-    
     try:
         json_response = response.json()
         print(json_response.get('id'))
