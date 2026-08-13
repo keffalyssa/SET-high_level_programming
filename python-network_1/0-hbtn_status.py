b3ca8052d8086e39f3de68a73a87deecdb6bb4c1
keffalyssa <keffalyssak@gmail.com>
2026-08-13 04:32:12 -0700

    Fix 0-hbtn_status.py to remove sys and use strict url with statement

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,14 +3,11 @@
 Module 0-hbtn_status
 Fetches https://alx-intranet.hbtn.io/status using urllib
 """
-import sys
 import urllib.request
 
 
 if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
-    if len(sys.argv) > 1:
-        url = sys.argv[1]
     with urllib.request.urlopen(url) as response:
         content = response.read()
         print("Body response:")
