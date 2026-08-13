76decfc19fccc5019fe69e5e6f344d3ab5c77211
keffalyssa <keffalyssak@gmail.com>
2026-08-13 04:24:22 -0700

    Fix 0-hbtn_status.py to strictly use static url and with statement

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
 """
 Module 0-hbtn_status
-Fetches a URL using urllib (handles both default and dynamic test URLs)
+Fetches https://alx-intranet.hbtn.io/status using urllib
 """
-import sys
 import urllib.request
 
 
 if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
-    if len(sys.argv) > 1:
-        url = sys.argv[1]
-
-    try:
-        with urllib.request.urlopen(url) as response:
-            content = response.read()
-            print("Body response:")
-            print(f"\t- type: {type(content)}")
-            print(f"\t- content: {content}")
-            print(f"\t- utf8 content: {content.decode('utf-8')}")
-    except Exception:
-        pass
+    with urllib.request.urlopen(url) as response:
+        content = response.read()
+        print("Body response:")
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
+        print(f"\t- utf8 content: {content.decode('utf-8')}")
