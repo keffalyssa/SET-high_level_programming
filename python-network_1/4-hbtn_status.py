9ba5c372592294f9d4d5359628c77398b0ccd9b9
keffalyssa <keffalyssak@gmail.com>
2026-08-13 04:45:33 -0700

    Fix 4-hbtn_status.py to remove sys and use only requests

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
 """
 Module 4-hbtn_status
-Fetches a status URL using the requests package, supporting optional arguments
+Fetches https://alx-intranet.hbtn.io/status using the requests package
 """
-import sys
 import requests
 
 
 if __name__ == '__main__':
     url = 'https://alx-intranet.hbtn.io/status'
-    if len(sys.argv) > 1:
-        url = sys.argv[1]
-    
     response = requests.get(url)
     content = response.text
     print("Body response:")
